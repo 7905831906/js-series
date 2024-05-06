90a06e7643c79615486b2a28b3f937143461f4a2
Nikhar Waris <87069959+7905831906@users.noreply.github.com>
2024-05-06 08:13:10 +0000

    Conversion Of datatypes

---
+++ 01_basics/02_conversionOperations.js
@@ -0,0 +1,39 @@
+//let score = "33abc"
+//let score = null
+//let score = undefined
+//let score = true
+let score = 'Nikhar'
+
+//console.log(typeof score)
+//console.log(typeof (score))//another way as method
+
+let valueInNumber = Number(score)//string to number 
+
+//console.log(typeof valueInNumber)
+//console.log( valueInNumber)//NaN for "33abc"
+                           // for null -value 0
+                           // for boolean-true- 1 & false-0 
+                           //for string in "Like"- NaN
+
+//"33"=>33
+//"33abc"=> NaN
+
+//let isLoggedIn =1
+//let isLoggedIn =''
+let isLoggedIn ="Nikhar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+//1 => true
+//0 => false
+//"Nikhar" => true
+//" " => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
