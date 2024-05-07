5f82f1d69a07686ff0d860cce2ab407416a7e8e7
Nikhar Waris <87069959+7905831906@users.noreply.github.com>
2024-05-07 06:17:50 +0000

    update

--- 01_basics/04_datatypesSummary.js
+++ 01_basics/04_datatypesSummary.js
@@ -14,10 +14,22 @@ let userEmail;//undefined
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);//false
+//console.log(id === anotherId);//false
 
-const bigNumber = 2356589564544456544564564;
+const bigNumber = 2356589564544456544564564n
  
 //ReferenceType//Non Primitive
 
 //Array, Objects, Functions
+
+const heroes = ["Ironman", "Superman", "Spiderman"]
+let obj =
+{
+   name:"Nikhar",
+   age:22
+}
+const myFunction= function (){
+       console.log("Hello World")
+}
+myFunction();
+console.log(typeof outsideTemp)
