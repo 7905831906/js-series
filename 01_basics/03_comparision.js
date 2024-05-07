60f55b31c816debe1cc0f6ff949a4974497c3f99
Nikhar Waris <87069959+7905831906@users.noreply.github.com>
2024-05-07 05:32:18 +0000

    datatype Summary update

--- 01_basics/03_comparision.js
+++ 01_basics/03_comparision.js
@@ -4,9 +4,10 @@
 //console.log(2==1)
 //console.log(2!=1)//
 
-// console.log(null > 0)
-// console.log(null == 0)
-// console.log(null >= 0)
+// console.log(null > 0)//result-false because null is convert into 0 and 0 is not graeter than 0
+
+// console.log(null == 0)//false-beacause null is not convert into 0
+// console.log(null >= 0)//true-null is convert into 0
 
 // console.log(undefined > 0)
 // console.log(undefined == 0)
---
+++ 01_basics/04_datatypesSummary.js
@@ -0,0 +1,23 @@
+// Primitive DataTypes//Call By value
+
+/* 7 types : String, Number, Boolean, null, undefined, Symbol 
+   BigInt
+*/
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;//undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);//false
+
+const bigNumber = 2356589564544456544564564;
+ 
+//ReferenceType//Non Primitive
+
+//Array, Objects, Functions
